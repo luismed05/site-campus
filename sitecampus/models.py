289624e9d7b7ccebab66d31460c39a967a4dd097
Luis Felipe <luis_medeiros96@yahoo.com.br>
2019-11-21 19:37:06 +0000

    Atualizações na model feita

--- sitecampus/models.py
+++ sitecampus/models.py
@@ -5,11 +5,25 @@ from modelchoices import Choices
 # model de Post
 class Post(models.Model):
 
+    EstadoPub = (
+        ('1' , 'Rascunho'),
+        ('2' , 'Em Revisão'),
+        ('3' , 'Pronto'),
+    )
+
     titulo = models.CharField(max_length=100)
 
     autor = models.ForeignKey('Autor', on_delete=models.SET_NULL, null=True)
 
+    editoria = models.ForeignKey('Editoria', on_delete=models.SET_NULL, null=True)
+
     conteudo = models.TextField(max_length=1000)
+
+    estado = models.CharField(max_length=1, choices=EstadoPub, blank=False, default="Rascunho", null=False)
+
+    semestre_publicação = models.CharField(max_length=6, blank=False, null=False, help_text=u"20xx/x")
+
+    sutia = models.TextField(max_length=200, blank=True, null=True)
 
     post_image = models.ImageField(upload_to='images/', blank=True, null=True)
 
@@ -20,16 +34,24 @@ class Post(models.Model):
         return reverse ('detalhe_post', args=[str(self.id)])
 
 
+# model de Editorias
+class Editoria(models.Model):
+
+    nome = models.CharField(max_length=100)
+
+    descricao = models.TextField(max_length=500)
+
+    def __str__ (self):
+        return self.nome
+
+    def get_asolute_url(self):
+        return reverse ('detalhe_editoria', args=[str(self.id)])
+
+
 # model de Autor   
 
 class Autor(models.Model):
 
-    SEMESTRE_CHOICES = (
-        ('1' , '2019/1'),
-        ('2' , '2018/2'),
-        ('3' , '2018/1'),
-        ('4' , '2017/2'),
-    )
 
     autor_avatar = models.ImageField(upload_to='assets/images', blank=True, null=True)
 
@@ -39,7 +61,7 @@ class Autor(models.Model):
 
     nome_completo = models.CharField(max_length=100)
 
-    semestre = models.CharField(max_length=1, choices=SEMESTRE_CHOICES, blank=False, null=False)
+    semestre = models.CharField(max_length=6, blank=False, null=False, help_text=u"20xx/x")
 
     class Meta:
         ordering = ['primeiro_nome', 'sobrenome']
